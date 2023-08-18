d6450f9c9ad0d1b828aad429d368ef6194f1e361
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-08-19 01:09:28 +0530

    responsive text

--- src/app/components/Resume.js
+++ src/app/components/Resume.js
@@ -28,8 +28,7 @@ const Resume = () => {
   return (
    <div className="bg-white dark:bg-black">
     <div className="pt-4 px-8 lg:px-36">
-      <h2 className="md:text-4xl text-2xl font-bold mb-4">Resume</h2>
-
+    <h2 className="md:text-4xl text-2xl font-semibold mb-4 text-center md:text-left">Resume</h2>
       <section className="mb-8">
         <h3 className="md:text-2xl text-xl font-semibold mb-4">Education</h3>
         <div className="md:text-justify text-center">
@@ -48,21 +47,17 @@ const Resume = () => {
 
     <div className="md:grid grid-cols-2 gap-8">
       <section className="mb-8">
-        <h3 className="text-2xl font-semibold mt-4 mb-4">Tech Stack</h3>
+        <h3 className="text-2xl font-semibold mt-4 mb-4 "> <span className="md:text-left">Tech Stack</span></h3>
         <div className="grid grid-cols-2 gap-4">
           {techStack.map((tech, index) => (
-            <div key={index} className="md:text-lg p-2 md:p-4 
-            flex justify-center items-center
-            border-0
-            rounded-lg
-            hover:bg-black
-            hover:text-white
-
-            dark:bg-black
-            dark:text-white
-            dark:hover:text-black
-            dark:hover:bg-white
- 
+            <div key={index} className="md:text-lg md:p-4 p-2  
+            flex justify-center items-center border-0 rounded-lg 
+            hover:bg-black 
+            hover:text-white  
+            dark:bg-black 
+            dark:text-white 
+            dark:hover:text-black 
+            dark:hover:bg-white  
             transition duration-100">
               {tech}
             </div>
@@ -71,21 +66,19 @@ const Resume = () => {
       </section>
 
       <section className="mb-8">
-        <h3 className="text-2xl font-semibold mt-4 mb-4">Skills</h3>
+        <h3 className="text-2xl font-semibold mt-4 mb-4 md:text-left">
+          <span className="md:text-left">Skills</span>
+        </h3>
         <div className="grid grid-cols-2 gap-4">
           {skills.map((skill, index) => (
-            <div key={index} className="md:text-lg md:p-4 p-2 
-            flex justify-center items-center
-            border-0
-            rounded-lg
-            hover:bg-black
-            hover:text-white
-
-            dark:bg-black
-            dark:text-white
-            dark:hover:text-black
-            dark:hover:bg-white
- 
+            <div key={index} className="md:text-lg md:p-4 p-2  
+            flex justify-center items-center border-0 rounded-lg 
+            hover:bg-black 
+            hover:text-white  
+            dark:bg-black 
+            dark:text-white 
+            dark:hover:text-black 
+            dark:hover:bg-white  
             transition duration-100">
               {skill}
             </div>
