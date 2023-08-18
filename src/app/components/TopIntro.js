a811c2b3dbe6c1bd6f80f7edb57149b4598c475c
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-08-19 01:32:31 +0530

    responsive text

--- src/app/components/TopIntro.js
+++ src/app/components/TopIntro.js
@@ -29,26 +29,29 @@ const TopIntro = () => {
     <main> 
       <GlobalStyle />
       <section className="flex flex-col justify-center items-center pt-8 pb-4 px-4 lg:px-18">
-      <h2 className="md:text-2xl text-4xl font-semibold mb-4">
-        <span className="md:text-2xl text-xl font-normal">Hey, </span> 
-      <span className="font-semibold md:text-4xl text-3xl ">
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      I'm P Sidharth</span></h2>
-      <div className="py-2 px-4 lg:px-18 md:text-center text-justify">
-      <p className="md:text-2xl text-lg mb-4 my-2 ">
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        I'm a Student, Developer passionate about [yet to write] 
-      </p>
-      <p className="md:text-2xl text-lg my-2 text-gray-700 dark:text-gray-300">
-        With a background in Python and JavaScript, I bring Front-End and Back-End to the table
-      </p>
-      <p className="md:text-2xl text-lg my-2 text-gray-600 dark:text-gray-300">
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        Let's connect and build great things together...
-        <span className="inline-flex opacity-50 text-2xl font-light blinking">|</span>
-      </p>
-    </div>
-    </section>
+        <h2 className="md:text-2xl text-4xl font-semibold mt-8">
+          <span className="md:text-2xl text-xl font-normal">Hey, </span> 
+          <span className="font-semibold md:text-4xl text-3xl ">
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          I'm P Sidharth</span></h2>
+          <div className="py-2 px-4 lg:px-18 md:text-center text-justify mt-2 mb-8">
+            <p className="md:text-2xl text-lg mb-4 my-2 ">
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              I'm a Student, Developer passionate about [yet to write] 
+            </p>
+            <p className="md:text-2xl text-lg my-2 text-gray-700 dark:text-gray-300">
+              With a background in Python and JavaScript, I bring Front-End and Back-End to the table
+            </p>
+            <p className="md:text-2xl text-lg my-2 text-gray-600 dark:text-gray-300">
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              Let's connect and build great things together...
+              <span className="inline-flex opacity-50 text-2xl font-light blinking">|</span>
+            </p>
+          </div>
+          <button className="mt-8 mb-8 bg-green-600 hover:bg-green-700 text-white py-2 px-8 rounded-3xl">
+          <span className="text-xl">Connect</span>
+          </button>
+      </section>
     </main>
     </div>
   );
