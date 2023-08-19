2deac5b9a5583714f9f6e58c85a0a1eebf4a85bf
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-08-19 12:16:19 +0530

    added colours

--- src/app/components/Resume.js
+++ src/app/components/Resume.js
@@ -30,25 +30,28 @@ const Resume = () => {
     <div className="pt-4 px-8 lg:px-36">
     <h2 className="md:text-4xl text-2xl font-semibold mt-4 mb-4 text-center md:text-left">Resume</h2>
       <section className="mb-8">
-        <h3 className="md:text-2xl text-xl font-semibold mb-4">Education</h3>
+        <h3 className="md:text-2xl text-xl font-semibold mb-4 dark:text-green-500 text-green-600">Education</h3>
         <div className="md:text-justify text-center">
-        <p>Bachelor of Technology in Computer Science</p>
+        <p>BTech in Computer Science</p>
         <p>GITAM University, Graduated in 2026</p>
         </div>
       </section>
 
       <section className="mb-8">
-        <h3 className="md:text-2xl text-xl font-semibold mb-4">Experience</h3>
+        <h3 className="md:text-2xl text-xl font-semibold mb-4 dark:text-green-500 text-green-600">Experience</h3>
         <div className="md:text-justify text-center">
-        <p>Software Engineer Intern</p>
-        <p>ABC Tech, Summer 20XX</p>
+        <p>Software Engineer</p>
+        <p>ABC Tech, Summer 2026</p>
         </div>
       </section>
 
     <div className="md:grid grid-cols-2 gap-8">
       <section className="mb-8">
         <h3 className="text-2xl font-semibold mt-4 mb-4 "> 
-        <span className="md:text-left lg:ml-3">Tech Stack</span></h3>
+        <span className="md:text-left lg:ml-3 
+        dark:text-blue-500
+        text-blue-600">
+          Tech Stack</span></h3>
         <div className="grid grid-cols-2 gap-4">
           {techStack.map((tech, index) => (
             <div key={index} className="md:text-lg md:p-4 p-2  
@@ -68,7 +71,9 @@ const Resume = () => {
 
       <section className="mb-8">
         <h3 className="text-2xl font-semibold mt-4 mb-4 md:text-left">
-          <span className="md:text-left lg:ml-3">Skills</span>
+          <span className="md:text-left lg:ml-3 
+          dark:text-blue-500
+        text-blue-600">Skills</span>
         </h3>
         <div className="grid grid-cols-2 gap-4">
           {skills.map((skill, index) => (
