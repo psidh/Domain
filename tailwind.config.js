f7668df7d337e0927036c7190037ba1aad42b554
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-08-20 22:08:08 +0530

    Helvetica added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        ambit: ['Ambit', 'sans-serif'],
+        ambit: ['Ambit', 'Helvetica','sans-serif'],
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
