d88a22350e5ee55fbe160013c5c33728a3a4d59a
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-08-20 15:20:36 +0530

    projects added

--- src/app/components/Blog.js
+++ src/app/components/Blog.js
@@ -9,7 +9,7 @@ const Blog = () => {
     <h2 className="md:text-4xl text-2xl font-semibold mb-8">Blog Posts</h2>
       <div className="grid gap-6">
         {blogPosts.map((post, index) => (
-          <div key={index} className="bg-white dark:bg-gray-900 rounded-lg shadow-md hover:shadow-xl p-6">
+          <div key={index} className="bg-white dark:bg-gray-900 rounded-lg transform hover:scale-105 transition duration-300 shadow-md hover:shadow-xl p-6">
             <h3 className="text-xl font-semibold mb-2 dark:text-gray-200">{post.title}</h3>
             <p className="text-gray-400 ">{post.date}</p>
             <p className="mt-2 dark:text-gray-200">{post.content}</p>
