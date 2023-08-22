8de39d9e8c36edff2cbeefe70eab3d7946425d38
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-08-22 09:14:58 +0530

    shadow animations

--- src/app/components/Blog.js
+++ src/app/components/Blog.js
@@ -21,7 +21,9 @@ const Blog = () => {
         {blogPosts.map((post, index) => (
           <div key={index} className=" dark:bg-gray-900 rounded-lg transform hover:scale-105 transition duration-300 shadow-md 
           shadow-green-400 hover:shadow-green-900 
-          dark:shadow-blue-400 p-6">
+          dark:shadow-blue-400 
+          dark:hover:shadow-blue-900 
+          p-6">
             <h3 className="text-xl font-semibold mb-2 dark:text-gray-200">{post.title}</h3>
             <p className="text-gray-400 ">{post.date}</p>
             <p className="mt-2 dark:text-gray-200">{post.content}</p>
