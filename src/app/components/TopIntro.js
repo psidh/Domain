16ab4228675ff8722abf859f482198cf48073341
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-08-22 21:59:58 +0530

    margin in connect button

--- src/app/components/TopIntro.js
+++ src/app/components/TopIntro.js
@@ -73,7 +73,7 @@ const TopIntro = () => {
               <span className="inline-flex opacity-50 text-2xl font-light blinking">|</span>
             </p>
           </div>     
-          <button className="mt-6  md:mt-4 mb-8 bg-green-500 hover:bg-black dark:hover:bg-white dark:hover:text-black  hover:text-white text-white py-2 px-8 rounded-3xl animate-pulse">
+          <button className="mt-8  md:mt-6 mb-8 bg-green-500 hover:bg-black dark:hover:bg-white dark:hover:text-black  hover:text-white text-white py-2 px-8 rounded-3xl animate-pulse">
           <span className="text-xl"> <a href="mailto:philkhanasidharth14@gmail.com"> Connect </a></span>
           </button>
       </section>
