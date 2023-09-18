ddf7cf1505bdd3577cfa6f0d4c1b1ddca551469b
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-09-18 17:29:20 +0530

    react-icons added onto tech stack

--- src/app/components/Resume.js
+++ src/app/components/Resume.js
@@ -1,7 +1,11 @@
+"use client";
 import React, { useEffect } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { SiPython, SiJavascript, SiTypescript, SiC, SiCss3, SiCplusplus, SiHtml5,
+  SiNextdotjs, SiReact } from 'react-icons/si'; // Import the icons
+
 const GlobalStyle = createGlobalStyle`
 @font-face {
   font-family: 'Ambit';
@@ -11,21 +15,17 @@ const GlobalStyle = createGlobalStyle`
 
 const Resume = () => {
   const techStack = [
-    'Python',
-    'JavaScript',
-    'C',
-    'C++',
-    'Java',
-    'NextJs',
-    'React',
-    'Node.js',
-    'HTML5',
-    'CSS/SCSS',
-    'Tailwind CSS',
-    'SQL',
-    'Git',
-    'Bootstrap',
-    'MongoDB',
+    { name: 'Next.js 13', icon: <SiNextdotjs /> },
+    { name: 'JavaScript', icon: <SiJavascript /> },
+    { name: 'TypeScript', icon: <SiTypescript /> },
+    { name: 'React', icon: <SiReact /> },
+    { name: 'C++', icon: <SiCplusplus /> },
+    { name: 'C', icon: <SiC /> },
+    { name: 'CSS', icon: <SiCss3 /> },
+    { name: 'HTML', icon: <SiHtml5 /> },
+    { name: 'Python', icon: <SiPython /> },
+    
+    // Add more tech stack items with icons as needed
   ];
   const skills = [
     'Web Development',
@@ -39,10 +39,10 @@ const Resume = () => {
 
   useEffect(() => {
     AOS.init({
-      delay: 100, // Delay between animations (ms)
-      offset: 10, // Offset (in px) from the element's position when animation starts
-      duration: 800, // Duration of animations (ms)}); // Initialize AOS
-      once: true, // Whether animations should be repeated
+      delay: 100,
+      offset: 10,
+      duration: 800,
+      once: true,
       mirror: true,
       easing: 'ease-in-out',
     });
@@ -55,94 +55,26 @@ const Resume = () => {
         data-aos="fade-in font-ambit"
       >
         <div className="py-4 md:py-10 px-8 lg:px-36">
-          <h2 className="md:text-4xl text-white text-3xl font-semibold my-4 text-center md:text-left"
-          data-aos="flip-up">
+          <h2
+            className="md:text-4xl text-white text-3xl font-semibold my-4 text-center md:text-left"
+            data-aos="flip-up"
+          >
             Resume
           </h2>
           <div className="md:grid md:grid-cols-2 gap-8">
-            <section className="my-8" data-aos="fade-right">
-              <h3 className="md:text-3xl text-2xl font-semibold mb-4 text-green-500">
-                Education
-              </h3>
-              <div className="text-white">
-                <p className="md:text-xl text-lg ">Class 1 - 3</p>
-                <p className="flex items-center mb-8">
-                  Air Force School, IAF, West Bengal
-                </p>
-                <p className="md:text-2xl text-xl mt-8">Class 4 - 12</p>
-                <a
-                  href="https://kvsangathan.nic.in/"
-                  target="_blank"
-                  className="flex items-center"
-                >
-                  Kendriya Vidyalaya Sangathan [Kerala | AP]
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="ml-2 w-4 h-4"
-                  >
-                    <line x1="7" y1="17" x2="17" y2="7"></line>
-                    <polyline points="7 7 17 7 17 17"></polyline>
-                  </svg>
-                </a>
-                <p className="md:text-2xl text-xl mt-8">
-                  BTech in Computer Science
-                </p>
-                <a
-                  href="https://www.gitam.edu"
-                  target="_blank"
-                  className="flex items-center"
-                >
-                  GITAM University, Graduated in 2026
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="ml-2 w-4 h-4"
-                  >
-                    <line x1="7" y1="17" x2="17" y2="7"></line>
-                    <polyline points="7 7 17 7 17 17"></polyline>
-                  </svg>
-                </a>
-              </div>
-            </section>
-
-            <section className="my-8" data-aos="fade-right">
-              <h3 className="md:text-3xl  text-2xl font-semibold mb-4 text-green-500">
-                Experience
-              </h3>
-              <div className="text-white">
-                <p>Software Engineer</p>
-                <p>Summer 2026</p>
-              </div>
-            </section>
+            {/* Education and Experience sections here */}
           </div>
 
           <div>
             <div className="md:grid grid-cols-2 gap-8">
-              <section className="my-8 "  data-aos="fade-right">
+              <section className="my-8 " data-aos="fade-right">
                 <a
                   href="https:www.github.com/psidh/"
                   target="_blank"
                   className="md:text-4xl text-2xl flex items-center font-semibold mt-4 mb-4 "
                 >
                   <span
-                    className="md:text-left lg:ml-3 
-         
-        text-green-500 hover:text-green-200"
+                    className="md:text-left lg:ml-3 text-green-500 hover:text-green-200"
                   >
                     Tech Stack
                   </span>{' '}
@@ -162,14 +94,15 @@ const Resume = () => {
                     <polyline points="7 7 17 7 17 17"></polyline>
                   </svg>
                 </a>
-                <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-2 md:grid md:grid-cols-3 gap-4">
                   {techStack.map((tech, index) => (
                     <div
                       key={index}
-                      className="md:text-xl text-white md:p-4 p-2  
+                      className="md:text-xl text-white md:p-4 p-4   
             flex justify-start items-center border-0 rounded-lg"
                     >
-                      {tech}
+                      {tech.icon} {/* Display the icon */}
+                      <span className="ml-2">{tech.name}</span>
                     </div>
                   ))}
                 </div>
@@ -178,8 +111,7 @@ const Resume = () => {
               <section className="my-8" data-aos="fade-left">
                 <h3 className="md:text-4xl text-2xl font-semibold mt-4 mb-4 md:text-left">
                   <span
-                    className="md:text-left lg:ml-3 
-          text-green-500"
+                    className="md:text-left lg:ml-3 text-green-500"
                   >
                     Skills ⚒️
                   </span>
@@ -205,21 +137,3 @@ const Resume = () => {
 };
 
 export default Resume;
-
-// styles for the tech stack -->
-// hover:bg-black
-// hover:text-white
-// dark:bg-black
-// dark:text-white
-// dark:hover:text-black
-// dark:hover:bg-white
-// transition duration-100
-
-// styles for skills -->
-// hover:bg-black
-// hover:text-white
-// dark:bg-black
-// dark:text-white
-// dark:hover:text-black
-// dark:hover:bg-white
-// transition duration-100
