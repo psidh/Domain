0780d808c13ee832e5d28fa5b830571ad70f1cd6
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-07 15:39:15 +0530

    colors changed, lets investigate but rest all same

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,66 +3,13 @@ import Skills from './(resume)/skills';
 import Tools from './(resume)/tools';
 import Languages from './(resume)/languages';
 import Frameworks from './(resume)/frameworks';
-import {SiPython, SiJavascript, SiTypescript, SiC, SiCss3, SiCplusplus, SiHtml5, SiNextdotjs, SiReact, SiExpress, SiJupyter,
-  SiMongodb, SiTailwindcss, SiBootstrap, SiGnubash, SiPandas, SiFirebase, SiSupabase, SiGit, SiGithub,
-  SiFigma, SiMui, SiVercel, SiOpenai, SiVisualstudiocode, SiPycharm, SiIntellijidea, SiWebstorm, SiPostman,} from 'react-icons/si'; // Import the icons
-import { GrMysql } from 'react-icons/gr';
-import { BiLogoJava, BiLogoFlask } from 'react-icons/bi';
-import { FaNodeJs } from 'react-icons/fa';
+import imports from '../utils/import';
 
 const Resume = () => {
-  const frameworks = [
-    { name: 'Next.js 13', icon: <SiNextdotjs /> },
-    { name: 'React', icon: <SiReact />, color: 'text-cyan-400' },
-    { name: 'Tailwind CSS', icon: <SiTailwindcss />, color: 'text-emerald-300'},
-    { name: 'Express', icon: <SiExpress />, color: 'text-yellow-500' },
-    { name: 'Node.js', icon: <FaNodeJs />, color: 'text-lime-500' },
-    { name: 'Mongodb', icon: <SiMongodb />, color: 'text-green-400' },
-    { name: 'BootStrap', icon: <SiBootstrap />, color: 'text-violet-400' },
-    { name: 'Jupyter', icon: <SiJupyter />, color: 'text-orange-500' },
-    { name: 'Pandas', icon: <SiPandas />, color: 'text-blue-500' },
-    { name: 'FireBase', icon: <SiFirebase />, color: 'text-amber-400' },
-    { name: 'SupaBase', icon: <SiSupabase />, color: 'text-green-400' },
-    { name: 'MySQL', icon: <GrMysql />, color: 'text-blue-200' },
-    { name: 'Flask', icon: <BiLogoFlask />, color: 'text-red-500' },
-    // Add more lang stack items with icons as needed
-  ];
-
-  const lang = [
-    { name: 'Bash', icon: <SiGnubash />, color: 'text-gray-500' },
-    { name: 'C++', icon: <SiCplusplus />, color: 'text-teal-400' },
-    { name: 'C', icon: <SiC />, color: 'text-gray-400' },
-    { name: 'Java', icon: <BiLogoJava />, color: 'text-orange-500' },
-    { name: 'Python', icon: <SiPython />, color: 'text-blue-300' },
-    { name: 'CSS', icon: <SiCss3 />, color: 'text-cyan-400' },
-    { name: 'HTML', icon: <SiHtml5 />, color: 'text-orange-500' },
-    { name: 'JavaScript', icon: <SiJavascript />, color: 'text-yellow-400' },
-    { name: 'TypeScript', icon: <SiTypescript />, color: 'text-blue-400' },
-  ];
-
-  const tools = [
-    { name: 'Git', icon: <SiGit />, color: 'text-orange-500' },
-    { name: 'GitHub', icon: <SiGithub />, color: 'text-gray-100' },
-    { name: 'Vercel', icon: <SiVercel />, color: 'text-white' },
-    { name: 'Figma', icon: <SiFigma />, color: 'text-red-500' },
-    { name: 'Material UI', icon: <SiMui />, color: 'text-blue-500' },
-    { name: 'ChatGPT', icon: <SiOpenai />, color: 'text-teal-200' },
-    { name: 'Visual Studio Code', icon: <SiVisualstudiocode />, color: 'text-sky-400'},
-    { name: 'PyCharm', icon: <SiPycharm />, color: 'text-lime-400' },
-    { name: 'WebStorm', icon: <SiWebstorm />, color: 'text-blue-400' },
-    { name: 'IntelliJ', icon: <SiIntellijidea />, color: 'text-red-500' },
-    { name: 'PostMan', icon: <SiPostman />, color: 'text-orange-500' },
-  ];
-
-  const skills = [
-    'Web Development',
-    'Front-End Development',
-    'Back-End Development',
-    'Problem Solving',
-    'Team Collaboration',
-    'UI/UX Design',
-    'Version Control',
-  ];
+  const frameworks = imports[0];
+  const lang = imports[1];
+  const tools = imports[2];
+  const skills = imports[3];
 
   return (
     <main id="resume">
@@ -76,10 +23,10 @@ const Resume = () => {
           </div>
           <div>
             <div className="md:grid grid-cols-2 gap-8">
-              <Frameworks frameworks={frameworks}/>
-              <Languages lang={lang}/>
-              <Tools tools={tools}/>
-              <Skills skills={skills}/>
+              <Frameworks frameworks={frameworks} />
+              <Languages lang={lang} />
+              <Tools tools={tools} />
+              <Skills skills={skills} />
             </div>
           </div>
         </div>
