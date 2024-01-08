ff011cc59785a319150f2109e1e8cf0540c40537
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-01-08 12:45:16 +0530

    font size updated

--- src/components/(resume)/education.jsx
+++ src/components/(resume)/education.jsx
@@ -2,17 +2,17 @@ const Education = () => {
   return (
     <>
     <section>
-        <h3 className="md:text-3xl text-2xl font-semibold mb-4 text-green-500">
+        <h3 className="text-4xl font-bold mb-4 text-green-500">
           Education
         </h3>
         <div className='flex justify-between items-center'>
           <div className="text-white">
-            <a href="https://kvsangathan.nic.in/" target="_blank" className="md:text-2xl text-xl mt-8">
+            <a href="https://kvsangathan.nic.in/" target="_blank" className="text-2xl mt-8 font-semibold">
               Kendriya Vidyalaya Sangathan
             </a>
             <p className='text-lg mt-2'>Class 10th and 12th</p>
             
-            <p className="md:text-2xl text-xl mt-8">BTech in Computer Science</p>
+            <p className="text-2xl mt-8 font-semibold">BTech in Computer Science</p>
             <a href="https://www.gitam.edu" target="_blank" className="flex items-center text-lg mt-2">
               GITAM University, Graduated in 2026
             </a>          
--- src/components/(resume)/experience.jsx
+++ src/components/(resume)/experience.jsx
@@ -4,21 +4,21 @@ export default function experience() {
   return (
     <div>
       <div>
-        <h3 className="md:text-3xl text-2xl font-semibold mb-4 text-green-500">
+        <h3 className="text-4xl font-bold mb-4 text-green-500">
           Experience
         </h3>
         <div className="text-white">
-          <a href="https://kvsangathan.nic.in/" target="_blank" className="md:text-2xl text-xl mt-8">
+          <a href="https://kvsangathan.nic.in/" target="_blank" className="text-2xl mt-8 font-semibold">
             Ananta LLP
           </a>
           <p className="md:text-xl text-lg">Core Developer</p>
 
-          <p className="md:text-2xl text-xl mt-8">Web Dev Lead</p>
-          <a href="https://www.gitam.edu" target="_blank" className="flex items-center text-xl mt-2">
+          <p className="text-2xl mt-8 font-semibold">Web Dev Lead</p>
+          <a href="https://www.gitam.edu" target="_blank" className="flex items-center text-lg mt-2">
             Google Developer Student Clubs | GITAM
           </a>
-          <p className="md:text-2xl text-xl mt-8">Finalist</p>
-          <a href="https://www.gitam.edu" target="_blank" className="flex items-center text-xl mt-2">
+          <p className="text-2xl mt-8 font-semibold">Finalist</p>
+          <a href="https://www.gitam.edu" target="_blank" className="flex items-center text-lg mt-2">
             Smart India Hackathon 2023
           </a> 
         </div>
