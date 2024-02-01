81c09560e77d5b23911ace3b4daea2b66ffce319
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-02-01 13:28:51 +0530

    source code button changed
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,30 @@
+'use client';
 import projectsData from 'src/app/data/projectsData.js';
 import '../app/styles.css';
+import { useRouter } from 'next/navigation';
 
 const Projects = () => {
+  const router = useRouter();
+
+  const projectPush = (link) => {
+    router.push(link);
+  };
   return (
     <main>
       <div className='w-full container mx-auto pt-4 pb-4 px-4 md:px-8 md:py-6'>
         <h2 className='text-white md:text-4xl text-3xl font-semibold my-4 text-center md:text-left mb-4'>
           Projects
         </h2>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 md:gap-4 lg:gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 md:gap-4 lg:gap-8'>
           {projectsData.map((project, index) => (
-            <a href={project.projectLink} key={index}>
+            <div
+              onClick={() => {
+                projectPush;
+              }}
+              key={index}
+            >
               <div
-                className='bg-[#1a1a1a] project-card
+                className='bg-[#1a1a1a] project-card hover:
                 rounded-lg p-4 transform transition duration-200 flex flex-col justify-between items-center h-full'
               >
                 <img
@@ -25,18 +37,16 @@ const Projects = () => {
                 </p>
 
                 <p className='text-gray-400 my-2'>{project.description}</p>
-                <div className='flex flex-row items-center justify-center'>
-                  <a
-                    href={project.githubLink}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='text-green-500 mt-2 transform hover:underline hover:underline-offset-2'
-                  >
-                    Source Code
-                  </a>
-                </div>
+                <a
+                  href={project.githubLink}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='hover:text-black hover:bg-white text-black bg-green-500 px-4 py-2 rounded-md text-center mt-4 w-full transform transition-all duration-150'
+                >
+                  Source Code
+                </a>
               </div>
-            </a>
+            </div>
           ))}
         </div>
       </div>
