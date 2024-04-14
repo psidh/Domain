be9a51443585c4d08b13270e1ca6b9fdc9043d08
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-04-14 13:12:49 +0530

    reduced imports

--- src/utils/import.jsx
+++ src/utils/import.jsx
@@ -4,42 +4,22 @@ import {
   SiTypescript,
   SiC,
   SiCss3,
-  SiCplusplus,
   SiHtml5,
   SiNextdotjs,
   SiReact,
   SiExpress,
-  SiJupyter,
   SiMongodb,
   SiTailwindcss,
-  SiBootstrap,
   SiGnubash,
-  SiPandas,
   SiFirebase,
   SiSupabase,
-  SiGit,
-  SiGithub,
-  SiDart,
-  SiFigma,
-  SiMui,
-  SiVercel,
-  SiOpenai,
-  SiVisualstudiocode,
-  SiPycharm,
-  SiIntellijidea,
-  SiWebstorm,
-  SiPostman,
-  SiFlutter,
-} from 'react-icons/si'; // Import the icons
-import { GrMysql } from 'react-icons/gr';
-import { FaVuejs } from 'react-icons/fa6';
-import { BiLogoJava, BiLogoFlask } from 'react-icons/bi';
+} from 'react-icons/si'; 
+import { BiLogoJava } from 'react-icons/bi';
 import { FaNodeJs } from 'react-icons/fa';
 
 const frameworks = [
   { name: 'Next.js', icon: <SiNextdotjs /> },
   { name: 'React.js', icon: <SiReact />,},
-  { name: 'Vue.js', icon: <FaVuejs />,},
   { name: 'Tailwind', icon: <SiTailwindcss />,},
   { name: 'Express', icon: <SiExpress />,  },
   { name: 'Node.js', icon: <FaNodeJs />, },
@@ -60,34 +40,7 @@ const lang = [
   { name: 'HTML', icon: <SiHtml5 />,  },
 ];
 
-const tools = [
-  { name: 'Git', icon: <SiGit />,  },
-  { name: 'GitHub', icon: <SiGithub />, },
-  { name: 'Vercel', icon: <SiVercel />,  },
-  { name: 'Figma', icon: <SiFigma />,  },
-  { name: 'Material UI', icon: <SiMui />,  },
-  { name: 'ChatGPT', icon: <SiOpenai /> },
-  {
-    name: 'Visual Studio Code',
-    icon: <SiVisualstudiocode />,
-    color: 'text-sky-400',
-  },
-  { name: 'PyCharm', icon: <SiPycharm />, },
-  { name: 'WebStorm', icon: <SiWebstorm />, },
-  { name: 'IntelliJ', icon: <SiIntellijidea />,  },
-  { name: 'PostMan', icon: <SiPostman />,  },
-];
 
-const skills = [
-  'Web Development',
-  'Front-End Development',
-  'Back-End Development',
-  'Problem Solving',
-  'Team Collaboration',
-  'UI/UX Design',
-  'Version Control',
-];
-
-const imports = [frameworks, lang, tools, skills];
+const imports = [frameworks, lang];
 
 export default imports;
