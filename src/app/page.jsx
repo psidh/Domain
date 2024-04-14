ee05581ae6097e61bfbf8042d344b23afa9e79ea
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-04-14 08:34:09 +0530

    typo mistake : Navbar => NavBar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-import NavBar from '../components/Navbar';
+import NavBar from '../components/NavBar';
 import TopIntro from '../components/TopIntro';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
