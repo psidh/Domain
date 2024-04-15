80da2f7897fe34da963cb2fdcc292a2a8e81f0bf
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-04-15 20:42:29 +0530

    UI changes to the project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,9 +18,9 @@ const Projects = () => {
           {projectsData
             .slice(0, showAllProjects ? projectsData.length : projectsLimit)
             .map((project, index) => (
-              <div key={index}>
+              <div key={index} className='my-4 mx-2'>
                 <div
-                  className=' project-card
+                  className='project-card
                 rounded-xl transform transition duration-200 flex flex-col justify-start items-center h-full'
                 >
                   <img
@@ -33,26 +33,37 @@ const Projects = () => {
                   </p>
                   <p className='text-gray-400 my-2'>{project.description}</p>
                   <div className='grow' />
-                  <a
-                    href={project.githubLink}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='hover:bg-green-950/50  border border-green-500
-                  rounded-full px-4 py-2 text-center mt-4 w-full transform transition-all duration-150'
-                  >
-                    Source Code
-                  </a>
+                  <div className='flex justify-between items-center w-full space-x-2'>
+                    <a
+                      href={project.projectLink}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='hover:bg-fuchsia-900/50  border border-fuchsia-500
+                  rounded-full px-4 py-1 text-center mt-4 w-full transform transition-all duration-150'
+                    >
+                      GitHub
+                    </a>
+                    <a
+                      href={project.githubLink}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='hover:bg-blue-900/50  border border-blue-500
+                  rounded-full px-4 py-1 text-center mt-4 w-full transform transition-all duration-150'
+                    >
+                      Link
+                    </a>
+                  </div>
                 </div>
               </div>
             ))}
         </div>
         {/* Render the "View All" button if there are more projects to show */}
         {projectsData.length > projectsLimit && !showAllProjects && (
-          <div className='my-8 flex flex-col md:flex-row items-center justify-center w-full px-16'>
+          <div className='my-8 flex flex-row items-center justify-center w-full px-16'>
             <div className='h-[1px] w-full  px-8 bg-gray-600'></div>
             <button
               onClick={() => setShowAllProjects(true)}
-              className='mx-8 shrink-0 bg-green-500 border font-semibold text-white px-16 text-center py-3 rounded-full'
+              className='mx-8 shrink-0 bg-green-500 font-semibold text-white px-16 text-center py-2 rounded-full'
             >
               Show All
             </button>
