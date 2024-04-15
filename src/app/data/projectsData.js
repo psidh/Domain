bf67a841a95f5a430707bb64281ad96553523816
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-04-15 15:33:01 +0530

    added projects

--- src/app/data/projectsData.js
+++ src/app/data/projectsData.js
@@ -8,29 +8,80 @@ const projectsData = [
     projectLink: 'https://spruce-dev.vercel.app',
   },
   {
-    title: 'Amazon Clone',
-    description: 'Clone of E-commerce website Amazon. Full-Stack Web Application',
+    title: 'Coal Rake Navigators',
+    description:
+      'At CRN Coal Rake Navigators, we specialize in revolutionizing coal logistics by real-time updates at railway sidings. By optimizing coal transportation routes and minimizing waiting times, we help companies save money',
+    image: '/projects/project12.png',
+    githubLink: 'https://github.com/psidh/CoalRake',
+    projectLink: 'https://coal-rake.vercel.app/',
+  },
+  {
+    title: 'Google DevFest 2023 RSVP',
+    description:
+      'A centralised website to handle all the RSVP of attendees for Google Developer Groups DevFest Vizag Edition 2023. Handling 500 attendees and issuing customized e-ticket with qr-code and details',
+    image: '/projects/project13.png',
+    githubLink: 'https://github.com/psidh/devFest23',
+    projectLink: 'https://devfest23-psi.vercel.app/',
+  },
+  {
+    title: 'GDSC - Task Submission Portal',
+    description:
+      'Web application to store all the tasks submitted by the candidates to apply for Google Developer Student Clubs Core Team Recruitment',
+    image: '/projects/project14.png',
+    githubLink: 'https://github.com/ManasMalla/task-submission-portal',
+    projectLink: 'https://task-submission-portal.vercel.app/',
+  },
+  {
+    title: 'E-Commerce Platform - Amazon',
+    description:
+      'Clone of Amazon website. Web App with features of authentication, search, cart, buy-now, wishlist, past orders',
     image: '/projects/project5.png',
     githubLink: 'https://github.com/psidh/Amazon-Clone',
     projectLink: 'https://amazon-clone-psidh.vercel.app/',
   },
+  {
+    title: 'Web Study Jams Portal',
+    description:
+      'Web Study Jams is an initiative by GDSC Gitam University, Visakhapatnam (VSKP), aimed at providing students with a platform to enhance their web development skills.',
+    image: '/projects/project10.png',
+    githubLink: 'https:github.com/psidh/Web-Study-Jams',
+    projectLink: 'https://web-study-jams.vercel.app/',
+  },
+
+  {
+    title: 'GDSC GITAM Website',
+    description:
+      'Google Developer Student Clubs Official Website',
+    image: '/projects/project15.png',
+    githubLink: 'https://github.com/ManasMalla/gdsc-gitam',
+    projectLink: 'https://gdsc-gitam.vercel.app/',
+  },
 
   {
     title: 'Netflix Clone',
-    description: 'Clone of Streaming company website Netflix. Full-Stack Web App with features of authentication, streaming, favorites, wishlists, search and exploring',
+    description:
+      'Clone of Streaming company website Netflix. Full-Stack Web App with features of authentication, streaming, favorites, wishlists, search and exploring',
     image: '/projects/project8.png',
     githubLink: 'https://github.com/psidh/Netflix-Clone',
     projectLink: 'https://netflix-psidh.vercel.app/',
   },
+  {
+    title: 'Cat Or Dog',
+    description:
+      'Deep Learning Keras model to identify whether the animal is Dog or Cat',
+    image: '/projects/project11.png',
+    githubLink: 'https://github.com/psidh/CatOrDog',
+    // projectLink: 'https://amazon-clone-psidh.vercel.app/',
+  },
 
   {
     title: 'Spotify Clone',
-    description: 'Clone of Music Streaming company website Netflix. Full-Stack Web App with features of authentication, music, favorites, artists, search and repeating songs you love ❤️',
+    description:
+      'Clone of Music Streaming company website Netflix. Full-Stack Web App with features of authentication, music, favorites, artists, search and repeating songs you love ❤️',
     image: '/projects/project9.png',
     githubLink: 'https://github.com/psidh/Spotify-Clone',
     projectLink: 'https://spotify-clone-psidh.vercel.app/',
   },
-
 
   {
     title: 'Ultra Community',
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   const [showAllProjects, setShowAllProjects] = useState(false);
 
   // Define the number of projects to display initially
-  const projectsLimit = 4;
+  const projectsLimit = 3;
 
   return (
     <main>
@@ -14,24 +14,25 @@ const Projects = () => {
         <h2 className='text-green-500 md:text-4xl text-3xl font-semibold mt-4 mb-8 text-center md:text-left ml-4'>
           Projects
         </h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 md:gap-4 lg:gap-8'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4 md:gap-4 lg:gap-8'>
           {projectsData
             .slice(0, showAllProjects ? projectsData.length : projectsLimit)
             .map((project, index) => (
               <div key={index}>
                 <div
-                  className='bg-[#242424] project-card hover:
-                rounded-xl p-6 transform transition duration-200 flex flex-col justify-between items-center h-full'
+                  className=' project-card
+                rounded-xl transform transition duration-200 flex flex-col justify-start items-center h-full'
                 >
                   <img
                     src={project.image}
                     alt={project.title}
-                    className='rounded-lg object-left-top object-cover w-64 h-56'
+                    className='rounded-md object-center object-cover w-full h-56'
                   />
                   <p className='text-xl  justify-center font-semibold my-4 flex items-center'>
                     {project.title}
                   </p>
                   <p className='text-gray-400 my-2'>{project.description}</p>
+                  <div className='grow' />
                   <a
                     href={project.githubLink}
                     target='_blank'
@@ -47,15 +48,15 @@ const Projects = () => {
         </div>
         {/* Render the "View All" button if there are more projects to show */}
         {projectsData.length > projectsLimit && !showAllProjects && (
-          <div className='my-8 flex flex-col md:flex-row items-center justify-center w-full'>
-            <div className='h-[1px] w-full md:w-auto px-8 bg-gray-600'></div>
+          <div className='my-8 flex flex-col md:flex-row items-center justify-center w-full px-16'>
+            <div className='h-[1px] w-full  px-8 bg-gray-600'></div>
             <button
               onClick={() => setShowAllProjects(true)}
-              className='text-green-500 border border-green-500 font-semibold hover:text-green-600 px-16 text-center py-3 rounded-full'
+              className='mx-8 shrink-0 bg-green-500 border font-semibold text-white px-16 text-center py-3 rounded-full'
             >
               Show All
             </button>
-            <div className='h-[1px] w-full md:w-auto px-8 bg-gray-600'></div>
+            <div className='h-[1px]  w-full  px-8 bg-gray-600'></div>
           </div>
         )}
       </div>
