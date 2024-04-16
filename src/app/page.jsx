23183936a76c47ff0501dd189991e9a3e7ee46aa
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-04-17 01:03:02 +0530

    experiment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import NavBar from '../components/NavBar';
 import TopIntro from '../components/TopIntro';
 import Projects from '../components/Projects';
@@ -5,10 +6,21 @@ import Skills from '../components/Skills';
 function MyApp() {
   return (
     <div>
-      <NavBar /> 
-      <TopIntro />
+      <div className='sm:hidden'>
+        <TopIntro />
+      </div>
+      <div className='hidden sm:flex sm:flex-col sm:items-center sm:justify-center'>
+        <div className=''>
+          <img
+            src='/Group.svg'
+            alt=''
+            className='h-full w-full md:w-[90%] md:h-[65%] object-cover'
+          />
+        </div>
+      </div>
       <Projects />
       <Skills />
+      <NavBar />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,30 +6,30 @@ import { FiGithub } from 'react-icons/fi';
 
 export default function  NavBar(){
   return (
-    <main id='footer'>
+    <main id='footer invisible sm:visible'>
       <div className=''>
         <div className='py-8 md:py-16 px-8 lg:px-36 border-t-[0.05em] flex lg:flex-row flex-col gap-12 lg:gap-0 justify-between items-center'>
           <div className='flex gap-12 '>
             <a
-              className='hover:scale-125 transition duration-150 text-2xl'
+              className='hover:scale-125 transition duration-150 text-xl'
               href='https://instagram.com/p_s1dharth'
             >
               <FaInstagram/>
             </a>
             <a
-              className='hover:scale-125 transition duration-150 text-2xl'
+              className='hover:scale-125 transition duration-150 text-xl'
               href='https://twitter.com/p_s1dharth'
             >
               <FiTwitter/>
             </a>
             <a
-              className='hover:scale-125 transition duration-150 text-2xl'
+              className='hover:scale-125 transition duration-150 text-xl'
               href='https://www.linkedin.com/in/p-sidharth'
             >
               <FiLinkedin/>
             </a>
             <a
-              className='hover:scale-125 transition duration-150 text-2xl'
+              className='hover:scale-125 transition duration-150 text-xl'
               href='https://github.com/psidh'
             >
               <FiGithub/>
